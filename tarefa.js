7041fe611602ccc1ce31188f44bcbf865ad3562f
Gustavo Barbosa <gbarbosagustavo01@gmail.com>
2023-05-25 19:52:51 -0300

    finalizando

---
+++ tarefa.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicacao(3, 7));
+console.log(saudacao('Gustavo'));
---
+++ tarefa.ts
@@ -0,0 +1,11 @@
+function multiplicacao(a: number, b: number): number { 
+    return a * b
+}
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`
+}
+
+console.log(multiplicacao(3,7))
+
+console.log(saudacao('Gustavo'))
